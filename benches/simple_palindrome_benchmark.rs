cb1755dcb873cbfaf600a9ce5321070cb90ccc3d
Eitan Adler <lists@eitanadler.com>
2022-12-29 00:45:22 +0000

    [bench] enable html reports

--- benches/simple_palindrome_benchmark.rs
+++ benches/simple_palindrome_benchmark.rs
@@ -13,10 +13,22 @@ pub fn criterion_dual_one_char_palindrome(c: &mut Criterion) {
     });
 }
 
+pub fn criterion_compare_simple_vs_dual(c: &mut Criterion) {
+    let mut group = c.benchmark_group("comparison one char");
+    group.bench_function("one char simple iterator palindrome)", |b| {
+        b.iter(|| bmark::simple_palindrome("a"))
+    });
+    group.bench_function("one char dual iterator palindrome)", |b| {
+        b.iter(|| bmark::dual_iterator_palindrome("a"))
+    });
+    group.finish();
+}
+
 criterion_group!(
     benches,
     criterion_simple_one_char_palindrome,
-    criterion_dual_one_char_palindrome
+    criterion_dual_one_char_palindrome,
+    criterion_compare_simple_vs_dual
 );
 // criterion_group!(benches, criterion_dual_one_char_palindrome);
 criterion_main!(benches);
